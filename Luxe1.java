917fba69fdb0204e436062c90677ddfa8721e218
ganeshwarn2001 <90825359+ganeshwarn2001@users.noreply.github.com>
2022-10-12 08:06:07 +0530

    Add files via upload

---
+++ Luxe1.java
@@ -0,0 +1,13 @@
+class Luxe1
+{
+String movieName;
+int time;
+boolean hit;
+
+public Luxe1(String mN,int t,boolean h)
+{
+movieName = movieName;
+time = time;
+hit = hit; 
+}
+}
---
+++ Phoenix1.java
@@ -0,0 +1,8 @@
+class Phoenix1
+{
+public static void main(String args[]){
+Luxe1 s1 = new Luxe1("PS1",10,true);
+System.out.println(s1.movieName);
+Luxe1 s2 = new Luxe1("NV",10,false);
+}
+}
