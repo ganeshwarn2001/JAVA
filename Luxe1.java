0de82ae1df09503cf26914319d723d3187a127e1
ganeshwarn2001 <90825359+ganeshwarn2001@users.noreply.github.com>
2022-10-12 08:11:43 +0530

    Update Luxe1.java

--- Luxe1.java
+++ Luxe1.java
@@ -11,3 +11,9 @@ time = time;
 hit = hit; 
 }
 }
+public static void main(String args[]){
+Luxe1 s1 = new Luxe1("PS1",10,true);
+System.out.println(s1.movieName);
+Luxe1 s2 = new Luxe1("NV",10,false);
+}
+}
